38c56777ad7f0d7d324bffa4e04943ca6ada1172
michudud <pzlc1236@gmail.com>
2023-03-31 16:05:53 +0200

    split content and style update

--- src/CartDetails.tsx
+++ src/CartDetails.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import DetailsTable from "./DetailsTable";
 import NavBar from "./NavBar";
 
 const CartDetails = ({}) => {
   const location = useLocation();
   const navigate = useNavigate();
   const cart = location.state;
+
   return (
     <div className="CartDetails">
       <NavBar />
@@ -21,55 +23,7 @@ const CartDetails = ({}) => {
           </button>
         </div>
         <div className="details">
-          <table>
-            <thead>
-              <tr>
-                <th>Product</th>
-                <th colSpan={3}>Price</th>
-                {/* <th style={{ width: "200px" }}></th> */}
-                <th colSpan={2}>Quantity</th>
-              </tr>
-            </thead>
-            <tbody>
-              {cart.products.map((product) => {
-                return (
-                  <tr>
-                    <td>{product.title}</td>
-                    <td style={{ textDecoration: "line-through" }}>
-                      {product.price}$
-                    </td>
-                    <td>{"=>"}</td>
-                    <td style={{ color: "red", fontWeight: 600 }}>
-                      {Math.round(
-                        (product.discountedPrice / product.quantity) * 100
-                      ) / 100}
-                      $
-                    </td>
-
-                    <td colSpan={2}>{product.quantity}</td>
-                  </tr>
-                );
-              })}
-              <tr style={{ fontWeight: 600 }}>
-                <td className="empty-noborder "></td>
-                <td className="empty-noborder "></td>
-                <td className="empty-noborder "></td>
-                <td className="empty-noborder "></td>
-                <td>Total Price</td>
-                <td style={{ textDecoration: "line-through" }}>
-                  {cart.total}$
-                </td>
-              </tr>
-              <tr style={{ fontWeight: 600, color: "red" }}>
-                <td className="empty-noborder "></td>
-                <td className="empty-noborder "></td>
-                <td className="empty-noborder "></td>
-                <td className="empty-noborder "></td>
-                <td>Discounted Price</td>
-                <td>{cart.discountedTotal}$</td>
-              </tr>
-            </tbody>
-          </table>
+          <DetailsTable cart={cart} />
         </div>
       </div>
     </div>
@@ -77,13 +31,3 @@ const CartDetails = ({}) => {
 };
 
 export default CartDetails;
-
-// {
-//   id: 39,
-//   title: "Women Sweaters Wool",
-//   price: 600,
-//   quantity: 2,
-//   total: 1200,
-//   discountPercentage: 17.2,
-//   discountedPrice: 994,
-// },
---
+++ src/DetailsTable.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const DetailsTable = ({ cart }) => {
+  return (
+    <table className="DetailsTable">
+      <thead>
+        <tr>
+          <th>Product</th>
+          <th></th>
+          <th>Price</th>
+          <th></th>
+          <th colSpan={2}>Quantity</th>
+        </tr>
+      </thead>
+      <tbody>
+        {cart.products.map((product) => {
+          return (
+            <tr>
+              <td>{product.title}</td>
+              <td style={{ textDecoration: "line-through" }}>
+                {product.price}$
+              </td>
+              <td>{"=>"}</td>
+              <td style={{ color: "red", fontWeight: 600 }}>
+                {Math.round(
+                  (product.discountedPrice / product.quantity) * 100
+                ) / 100}
+                $
+              </td>
+
+              <td colSpan={2}>{product.quantity}</td>
+            </tr>
+          );
+        })}
+        <tr style={{ fontWeight: 600 }}>
+          <td className="empty-noborder"></td>
+          <td className="empty-noborder"></td>
+          <td className="empty-noborder"></td>
+          <td className="empty-noborder"></td>
+          <td>Total Price</td>
+          <td style={{ textDecoration: "line-through" }}>{cart.total}$</td>
+        </tr>
+        <tr style={{ fontWeight: 600, color: "red" }}>
+          <td className="empty-noborder"></td>
+          <td className="empty-noborder"></td>
+          <td className="empty-noborder"></td>
+          <td className="empty-noborder"></td>
+          <td>Discounted Price</td>
+          <td>{cart.discountedTotal}$</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default DetailsTable;
