8839fc54947e5af9791e792df8b57c251d23be79
michudud <pzlc1236@gmail.com>
2023-04-02 17:29:29 +0200

    footer fix

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
-  const navigate = useNavigate();
   return (
     <footer>
       <p>michudud 2023</p>
